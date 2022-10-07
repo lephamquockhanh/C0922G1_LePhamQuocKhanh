73519025267c0ca75253a914e962a7ab07fb4c83
quoc khanh le pham <khanhle02022001@gmail.com>
2022-10-07 13:51:25 +0700

    Bai tap 7/10

---
+++ module1/ss7_Bien_Kieu_du_lieu_Toan_tu/Bai_tap/BKdlTt2.js
@@ -0,0 +1,4 @@
+let Do_C;
+Do_C=parseFloat(prompt('Nhập Độ C'));
+let Do_F = 32+(Do_C/5)*9;
+document.write("Độ F = " +Do_F);
---
+++ module1/ss7_Bien_Kieu_du_lieu_Toan_tu/Bai_tap/BKldTt4.js
@@ -0,0 +1,4 @@
+let Ban_kinh;
+Ban_kinh = parseFloat(prompt("Nhập Bán Kính"));
+let Chu_vi = (Ban_kinh*2) *3.14;
+document.write("Chu vi = "+Chu_vi);
